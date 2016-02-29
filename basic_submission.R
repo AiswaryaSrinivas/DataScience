b7bc8da3d3b7782afdc86baa4d6c5630c60bc8b7
Aiswarya Srinivas <aiswaryar2010@gmail.com>
2016-02-29 11:22:21 +0530

    Delete basic_submission.R

--- basic_submission.R
+++
@@ -1,9 +0,0 @@
-# The most common kind of crime is Larceny/Theft, so predict Larceny/Theft for all the test dat
-train<-read.csv("train.csv")
-test<-read.csv("test.csv")
-sub<-read.csv("sampleSubmission.csv")
-sub$Id<-test$Id
-sub$WARRANTS<-0
-sub$LARCENY.THEFT<-1
-write.csv(sub,"basic_submission.csv")
-
